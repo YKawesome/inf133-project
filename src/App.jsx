ee51600f4197df94a6a89020f4074c6f1ad52843
Yousef <theyousefkhan@gmail.com>
2024-11-27 20:33:30 -0800

    A4

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
 import "./App.css";
+import Footer from "./components/footer/footer";
+import Login from "./components/login/login";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
+      <Login />
+      <Footer />
     </>
   );
 }
