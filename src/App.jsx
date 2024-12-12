1f5d8bdbe720255bf3c8543e475af2c230e2c4b1
Yousef <theyousefkhan@gmail.com>
2024-12-11 22:33:06 -0800

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 import Footer from "./components/footer/footer";
 import Login from "./components/login/login";
 import Navbar from "./components/navbar/navbar";
+import Todo from "./components/todo/todo";
+
+
 
 function App() {
   return (
     <>
       <Navbar />
-      <Login />
+      {/* <Login /> */}
+      <Todo />
       <Footer />
     </>
   );
